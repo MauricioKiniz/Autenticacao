4e300b18940183e3d770621df433030268ce7e95
Mauricio <mksistemaseconsultoria@gmail.com>
2018-05-19 15:27:22 -0300

    Salvar dados

---
+++ MKSistemas.MicrosServico.Autenticacao/Dominio/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MKSistemas.MicrosServico.Autenticacao.Dominio
+{
+    public class Usuario
+    {
+        public Guid Id { get; }
+        public string Nome { get; }
+        public string Senha { get; private set; }
+
+        private Usuario(Guid Id, string nome)
+        {
+            Nome = nome;
+        }
+
+        public void AlterarSenha(string novaSenha)
+        {
+            Senha = novaSenha;
+        }
+
+        public static Usuario Create(Guid id, string nome, string senha)
+        {
+            Usuario usuario = new Usuario(id, nome);
+            usuario.Senha = senha;
+
+            return usuario;
+        }
+    }
+}
